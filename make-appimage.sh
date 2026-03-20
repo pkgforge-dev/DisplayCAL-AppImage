d2c252186b7ce40962e0e0eb6bf625510435bf69
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-20 14:38:44 -0400

    pain

--- make-appimage.sh
+++ make-appimage.sh
@@ -13,11 +13,27 @@ export DESKTOP=/usr/share/applications/displaycal.desktop
 export DEPLOY_PYTHON=1
 export DEPLOY_SDL=1
 
+# we have to passs a ton of binaries from the argyllcms package
+bins='
+	applycal average cb2ti3 cctiff ccxxmake chartread collink colprof
+	colverify cxf2ti3 dispcal dispread dispwin extracticc extractttag
+	fakeCMY fakeread greytiff iccdump iccgamut icclu iccvcgt illumread
+	invprofcheck kodak2ti3 ls2ti3 mppcheck mpplu mppprof oeminst printcal
+	printtarg profcheck refine revfix scanin spec2cie specplot splitti3
+	spotread synthcal synthread targen tiffgamut timage txt2ti3 viewgam xicclu
+'
+set --
+for bin in $bins; do
+	set -- /usr/bin/"$bin" "$@"
+done
+
 # Deploy dependencies
 quick-sharun \
 	/usr/bin/displaycal* \
 	/usr/share/DisplayCAL \
-	/usr/share/argyllcms
+	/usr/share/argyllcms  \
+	"$@"
+
 
 # Additional changes can be done in between here
 
